e7078ea041bc13f61601efae90d1a1ee7ff9bc34
Hooriya Amjad <hooriyaamjad@Hooriyas-MacBook-Pro.local>
2023-04-12 02:39:19 -0600

    working on obituary screen

--- src/NewObituaryScreen.js
+++ src/NewObituaryScreen.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 
 function NewObituaryScreen() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [name, setName] = useState("");
+  const [bornDate, setBornDate] = useState("");
+  const [diedDate, setDiedDate] = useState("");
 
   const handleImageSelect = (e) => {
     const file = e.target.files[0];
@@ -21,13 +24,14 @@ function NewObituaryScreen() {
         <div id="obit-holder">
           <div id="obit-maker">
             <h3>Create a New Obituary</h3>
-            <br />
             {/* image isnt working? */}
             <img src="./obituary.jpg" alt="obituary" />
             <br />
+            <br />
             <div id="image-input">
               <label for="image-select">
-                Select an image for the deceased ({selectedImage && <span>{selectedImage.name}</span>})
+                Select an image for the deceased:{" "}
+                {selectedImage && <span>{selectedImage.name}</span>}
               </label>
               <input
                 id="image-select"
@@ -35,6 +39,33 @@ function NewObituaryScreen() {
                 accept="image/*"
                 onChange={handleImageSelect}
                 style={{ display: "none" }}
+              />
+            </div>
+            <br />
+            <div>
+              <label id="name">Name:</label>
+              <input
+                id="name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <br />
+            <div>
+              <label id="born-date">Born: </label>
+              <input
+                type="datetime-local"
+                value={bornDate}
+                onChange={(e) => setBornDate(e.target.value)}
+              />
+              <label id="died-date" style={{ marginLeft: "30px" }}>
+                Died:{" "}
+              </label>
+              <input
+                type="datetime-local"
+                value={diedDate}
+                onChange={(e) => setDiedDate(e.target.value)}
               />
             </div>
           </div>
